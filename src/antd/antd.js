26d4c9138e7d1154035a46aa463a4caeafe4d359
liuxi <liuxi@apexsoft.com.cn>
2018-01-02 16:41:41 +0800

    添加antd 模块引入

---
+++ src/antd/antd.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import { Layout, Menu, Breadcrumb, Icon } from 'antd';
+import 'antd/dist/antd.css';  // or 'antd/dist/antd.less'
+const { Header, Content, Footer, Sider } = Layout;
+const SubMenu = Menu.SubMenu;
+
+export default class Antd extends Component {
+    state = {
+        collapsed: false,
+    };
+    onCollapse = (collapsed) => {
+        console.log(collapsed);
+        this.setState({ collapsed });
+    }
+    showKey = (e) =>{
+        console.log(e.key);
+        // document.getElementsByClassName("content").html(e.key);
+    }
+    render() {
+        return (
+            <Layout style={{ minHeight: '100vh' }}>
+                <Sider
+                    collapsible
+                    collapsed={this.state.collapsed}
+                    onCollapse={this.onCollapse}
+                >
+                    <div className="logo" />
+                    <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline"
+                          onClick={this.showKey}>
+                        <Menu.Item key="1">
+                            <Icon type="pie-chart" />
+                            <span>Option 1</span>
+                        </Menu.Item>
+                        <Menu.Item key="2">
+                            <Icon type="desktop" />
+                            <span>Option 2</span>
+                        </Menu.Item>
+                        <SubMenu
+                            key="sub1"
+                            title={<span><Icon type="user" /><span>User</span></span>}
+                        >
+                            <Menu.Item key="3">Tom</Menu.Item>
+                            <Menu.Item key="4">Bill</Menu.Item>
+                            <Menu.Item key="5">Alex</Menu.Item>
+                        </SubMenu>
+                        <SubMenu
+                            key="sub2"
+                            title={<span><Icon type="team" /><span>Team</span></span>}
+                        >
+                            <Menu.Item key="6">Team 1</Menu.Item>
+                            <Menu.Item key="8">Team 2</Menu.Item>
+                        </SubMenu>
+                        <Menu.Item key="9">
+                            <Icon type="file" />
+                            <span>File</span>
+                        </Menu.Item>
+                    </Menu>
+                </Sider>
+                <Layout>
+                    <Header style={{ background: '#fff', padding: 0 }} />
+                    <Content style={{ margin: '0 16px' }}>
+                        <Breadcrumb style={{ margin: '16px 0' }}>
+                            <Breadcrumb.Item>User</Breadcrumb.Item>
+                            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+                        </Breadcrumb>
+                        <div style={{ padding: 24, background: '#fff', minHeight: 600 }}>
+                            <div className="content"></div>
+                        </div>
+                    </Content>
+                    <Footer style={{ textAlign: 'center' }}>
+
+                    </Footer>
+                </Layout>
+            </Layout>
+        );
+    }
+}
+
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Antd from './antd/antd';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Antd />, document.getElementById('root'));
 registerServiceWorker();
